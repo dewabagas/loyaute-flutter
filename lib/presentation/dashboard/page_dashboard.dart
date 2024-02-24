43a339b5cc4e147127c1c48731537b84c87a5273
Bagas Dewa <dewabagasql@gmail.com>
2024-02-24 23:58:46 +0700

    fix navbar

--- lib/presentation/dashboard/page_dashboard.dart
+++ lib/presentation/dashboard/page_dashboard.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:loyaute/presentation/core/constants/assets.dart';
+import 'package:loyaute/presentation/core/constants/styles.dart';
 import 'package:loyaute/presentation/core/styles/app_colors.dart';
 import 'package:loyaute/presentation/dashboard/tabs/tab_history.dart';
 import 'package:loyaute/presentation/dashboard/tabs/tab_home.dart';
 import 'package:loyaute/presentation/dashboard/tabs/tab_profile.dart';
-import 'package:loyaute/presentation/shared/others/icon_bottom_nav_bar.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class PageDashboard extends StatefulWidget {
   final int? tabIndex;
-  const PageDashboard({super.key, this.tabIndex});
+  const PageDashboard({Key? key, this.tabIndex}) : super(key: key);
 
   @override
   State<PageDashboard> createState() => _PageDashboardState();
@@ -18,60 +21,89 @@ class PageDashboard extends StatefulWidget {
 class _PageDashboardState extends State<PageDashboard> {
   int _currentIndex = 0;
 
-  final List<IconBottomNavBarItem> items = [
-    IconBottomNavBarItem(
-        iconActive: BottomBar.icHomeActive,
-        iconInactive: BottomBar.icHome,
-        label: 'Home'),
-    IconBottomNavBarItem(
-        iconActive: BottomBar.icHistoryActive,
-        iconInactive: BottomBar.icHistory,
-        label: 'History'),
-    IconBottomNavBarItem(
-        iconActive: BottomBar.icInboxActive,
-        iconInactive: BottomBar.icInbox,
-        label: 'Inbox'),
-    IconBottomNavBarItem(
-        iconActive: BottomBar.icProfileActive,
-        iconInactive: BottomBar.icProfile,
-        label: 'Profile'),
-  ];
-
-  static final List widgetOptions = [
-    const TabHome(),
-    TabHistory(),
-    const TabHome(),
-    const TabProfile(),
-  ];
-
-  void _onItemTapped(int? index) {
-    setState(() {
-      _currentIndex = index!;
-    });
-  }
-
   @override
   void initState() {
-    FlutterNativeSplash.remove();
-    initIndex();
     super.initState();
+    _currentIndex = widget.tabIndex ?? 0;
+    FlutterNativeSplash.remove();
   }
 
-  void initIndex() {
-    _currentIndex = widget.tabIndex ?? 0;
+  List<PersistentBottomNavBarItem> _navBarItems() {
+    return [
+      PersistentBottomNavBarItem(
+          icon: SvgPicture.asset(BottomBar.icHomeActive),
+          inactiveIcon: SvgPicture.asset(BottomBar.icHome),
+          title: 'Home',
+          textStyle:
+              TextStyles.bodyMedium.copyWith(fontWeight: FontWeight.w400)),
+      PersistentBottomNavBarItem(
+          icon: SvgPicture.asset(BottomBar.icHistoryActive),
+          inactiveIcon: SvgPicture.asset(BottomBar.icHistory),
+          title: 'History',
+          textStyle:
+              TextStyles.bodyMedium.copyWith(fontWeight: FontWeight.w400)),
+      PersistentBottomNavBarItem(
+          icon: SvgPicture.asset(BottomBar.icInboxActive),
+          inactiveIcon: SvgPicture.asset(BottomBar.icInbox),
+          title: 'Inbox',
+          textStyle:
+              TextStyles.bodyMedium.copyWith(fontWeight: FontWeight.w400)),
+      PersistentBottomNavBarItem(
+          icon: SvgPicture.asset(BottomBar.icProfileActive),
+          inactiveIcon: SvgPicture.asset(BottomBar.icProfile),
+          title: 'Profile',
+          textStyle:
+              TextStyles.bodyMedium.copyWith(fontWeight: FontWeight.w400)),
+    ];
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      const TabHome(),
+      TabHistory(),
+      const TabHome(),
+      const TabProfile(),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: widgetOptions.elementAt(_currentIndex),
-      bottomNavigationBar: IconBottomAppNavBar(
-        backgroundColor: AppColors.white,
-        notchedShape: const CircularNotchedRectangle(),
-        onTabSelected: _onItemTapped,
-        initialPage: _currentIndex,
-        items: items,
+    return PersistentTabView(
+      context,
+      controller: PersistentTabController(initialIndex: _currentIndex),
+      screens: _buildScreens(),
+      items: _navBarItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset: true,
+      stateManagement: true,
+      hideNavigationBarWhenKeyboardShows: true,
+      decoration: NavBarDecoration(
+        colorBehindNavBar: Colors.transparent,
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, -5),
+            blurRadius: 14,
+            spreadRadius: 0,
+            color: const Color(0xFFCBCDCE).withOpacity(0.18),
+          ),
+        ],
       ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      navBarHeight: 83.h,
+      itemAnimationProperties: const ItemAnimationProperties(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.simple, // Choose the nav bar style with this property
     );
   }
 }
